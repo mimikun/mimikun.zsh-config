08e351bd780bc746d0c4c67812b47fcba3126f89
Yuto Tanaka <git@mimikun.dev>
2026-05-05 14:42:33 +0900

    feat(env): add some envs

--- conf.d/env.zsh
+++ conf.d/env.zsh
@@ -193,4 +193,13 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 # Browser-Use
 export PATH="$HOME/.browser-use-env/bin:$HOME/.local/bin:$PATH"
 
+# https://donottrack.sh
+export DO_NOT_TRACK=1
+
+# https://force-color.org
+export FORCE_COLOR=1
+
+# https://no-color.org
+export NO_COLOR=0
+
 # vim:ft=zsh
