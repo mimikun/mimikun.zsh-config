735d19aadbef29af14415a9bbf4c93f533fd7008
Yuto Tanaka <git@mimikun.dev>
2026-05-05 14:55:05 +0900

    chore: comment-out some envs

--- conf.d/env.zsh
+++ conf.d/env.zsh
@@ -194,12 +194,12 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 export PATH="$HOME/.browser-use-env/bin:$HOME/.local/bin:$PATH"
 
 # https://donottrack.sh
-export DO_NOT_TRACK=1
+#export DO_NOT_TRACK=1
 
 # https://force-color.org
-export FORCE_COLOR=1
+#export FORCE_COLOR=1
 
 # https://no-color.org
-export NO_COLOR=0
+#export NO_COLOR=0
 
 # vim:ft=zsh
